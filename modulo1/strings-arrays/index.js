1c442d22416fda39c4f6068842b347465798a9e8
enzopasche <Enzo Pasche>
2021-12-01 15:34:58 -0300

    strings-e-arrays

---
+++ modulo1/strings-arrays/index.js
@@ -0,0 +1,32 @@
+//------------------Exercícios de interpretação de código------------------//
+
+//1. a)a.undefined
+//   b)b.null
+//   c)c.11
+//   d)d.3
+//   e)e.(11)
+//   f)f.9
+
+
+//2. SUBI NUM ÔNIBUS EM MIRROCOS 27
+//   
+
+/*const frase = prompt("Digite uma frase")
+
+console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)*/
+
+
+//------------------Exercícios de escrita de código------------------//
+
+//1.
+
+/*let nomeDoUsuario = prompt("Qual o seu nome?")
+let emailDoUsuario = prompt("Qual o seu email?")
+console.log(`O e-mail ${emailDoUsuario} foi cadastrado com sucesso. Seja bem-vinda(o), ${nomeDoUsuario}`)*/
+
+//2.
+
+/*let comidasPreferidas = ["Frango empanado, Churrasco, Pizza, Hambúrger, Peixe"]
+console.log(comidasPreferidas)
+console.log("Essas são as minhas comidas preferidas:")
+console.log(comidasPreferidas[])*/
