9f315bc1a4b84fd05b6a86c0ba835026b09d9d63
enzopasche <Enzo Pasche>
2021-12-01 19:00:41 -0300

    strings-e-arrays-feito

--- modulo1/strings-arrays/index.js
+++ modulo1/strings-arrays/index.js
@@ -11,22 +11,43 @@
 //2. SUBI NUM ÔNIBUS EM MIRROCOS 27
 //   
 
-/*const frase = prompt("Digite uma frase")
-
-console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)*/
-
-
 //------------------Exercícios de escrita de código------------------//
 
 //1.
 
-/*let nomeDoUsuario = prompt("Qual o seu nome?")
+let nomeDoUsuario = prompt("Qual o seu nome?")
 let emailDoUsuario = prompt("Qual o seu email?")
-console.log(`O e-mail ${emailDoUsuario} foi cadastrado com sucesso. Seja bem-vinda(o), ${nomeDoUsuario}`)*/
+console.log(`O e-mail ${emailDoUsuario} foi cadastrado com sucesso. Seja bem-vinda(o), ${nomeDoUsuario}`)
 
 //2.
 
-/*let comidasPreferidas = ["Frango empanado, Churrasco, Pizza, Hambúrger, Peixe"]
+const comidasPreferidas = ["Frango empanado", "Churrasco", "Pizza", "Hambúrger", "Peixe"]
 console.log(comidasPreferidas)
 console.log("Essas são as minhas comidas preferidas:")
-console.log(comidasPreferidas[])*/
+console.log(comidasPreferidas[0])
+console.log(comidasPreferidas[1])
+console.log(comidasPreferidas[2])
+console.log(comidasPreferidas[3])
+console.log(comidasPreferidas[4])
+
+let comidaUsuario = prompt("Qual é a sua comida favorita?")
+comidasPreferidas[1] = comidaUsuario
+console.log(comidasPreferidas)
+
+
+//3.
+
+
+
+let listaDeTarefas = [0]
+let tarefa1 = prompt("Diga uma tarefa que você precisa realizar no dia.")
+let tarefa2 = prompt("Diga uma segunda tarefa que você precisa realizar no dia.")
+let tarefa3 = prompt("Diga uma terceira tarefa que você precisa realizar no dia.")
+listaDeTarefas.push(tarefa1, tarefa2, tarefa3)
+console.log(listaDeTarefas)
+let perguntaUsuario = prompt("Digite o índice de uma tarefa que você já realizou.")
+console.log(perguntaUsuario)
+listaDeTarefas.splice(1, 1)
+console.log(listaDeTarefas)
+
+
