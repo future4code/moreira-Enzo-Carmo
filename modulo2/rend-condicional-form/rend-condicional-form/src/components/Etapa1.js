ce957ebd57e7997b7dfe055cf54098c7054405af
enzopasche <Enzo Pasche>
2022-01-17 19:40:07 -0300

    exercicio rend-condicional-form

---
+++ modulo2/rend-condicional-form/rend-condicional-form/src/components/Etapa1.js
@@ -0,0 +1,29 @@
+import React from "react"
+
+
+
+
+
+function Etapa1() {
+    return (
+      <div>
+        <h1>ETAPA 1 - DADOS GERAIS</h1>
+        <h4>1. Qual o seu nome?</h4>
+        <input></input>
+        <h4>2. Qual  sua idade?</h4>
+        <input></input>
+        <h4>3. Qual seu email?</h4>
+        <input></input>
+        <h4>4. Qual sua escolaridade?</h4>
+        <select>
+            <option>Ensino Médio Incompleto</option>
+            <option>Ensino Médio Completo</option>
+            <option>Ensino Superior Incompleto</option>
+            <option>Ensino Superior Incompleto</option>
+        </select>
+
+      </div>
+    );
+  }
+  
+  export default Etapa1;
