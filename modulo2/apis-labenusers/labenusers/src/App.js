404b1b249689ac4bb52554915f15cc5109183245
enzopasche <Enzo Pasche>
2022-01-25 20:50:30 -0300

    inicio do exercicio

---
+++ modulo2/apis-labenusers/labenusers/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import axios from 'axios';
+
+
+const urlUsers= "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users"
+
+const headers = {
+  headers: {
+    Authorization: "enzo-pasche-moreira"
+  }
+};
+
+export default class App extends React.Component {
+  render(){
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+  
+}
+
+
