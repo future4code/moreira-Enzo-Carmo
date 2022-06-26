b4b006a101f288f6c1a5342a70d2b88cef1ae136
enzopasche <Enzo Pasche>
2022-06-26 13:02:47 -0300

    case doghero

--- modulo6/iwfs-case-doghero-4/src/index.ts
+++ modulo6/iwfs-case-doghero-4/src/index.ts
@@ -1,33 +1,12 @@
-import express, {Express} from 'express'
-import cors from 'cors'
-import knex from "knex";
-import dotenv from "dotenv";
+import DogheroBusiness from "./Business/DogHeroBusiness";
+import app from "./Controller/App";
+import DogheroController from "./Controller/DogHeroController";
+import { DogheroData } from "./data/DogHeroData";
+import { PetsData } from "./data/PetsData";
 
-dotenv.config();
+const dogheroBusiness = new DogheroBusiness(new DogheroData(), new PetsData());
+const dogheroController = new DogheroController(dogheroBusiness);
 
-const app: Express = express();
-
-app.use(express.json());
-app.use(cors());
-
-import { AddressInfo } from "net";
-
-const server = app.listen(process.env.PORT || 3003, () => {
-    if (server) {
-       const address = server.address() as AddressInfo;
-       console.log(`Server is running in http://localhost: ${address.port}`);
-    } else {
-       console.error(`Failure upon starting server.`);
-    }
-});
-
-export const connection = knex({
-	client: "mysql",
-	connection: {
-    host: process.env.DB_HOST,
-    port: 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
-  }
-});
+app.post("/dogwalk/create", dogheroController.create);
+app.get("/dogwalk/index", dogheroController.index);
+app.put("/dogwalk/start", dogheroController.start);
